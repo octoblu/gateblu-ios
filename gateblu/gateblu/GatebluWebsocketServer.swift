f853c01a10136a2600f19b57b600ceac87c94c23
Jade Meskill <jade.meskill@gmail.com>
2014-12-19 11:33:10 -0700

    move bluetooth logic out of viewcontroller

---
+++ gateblu/gateblu/GatebluWebsocketServer.swift
@@ -0,0 +1,22 @@
+//
+//  GatebluWebsocketServer.swift
+//  gateblu
+//
+//  Created by Jade Meskill on 12/19/14.
+//  Copyright (c) 2014 Octoblu. All rights reserved.
+//
+
+import Foundation
+
+class GatebluWebsocketServer {
+    var server = BLWebSocketsServer.sharedInstance()
+
+    func start(handleRequest: (data:NSData!) -> (NSData!), onCompletion: (error: NSError!)->()) {
+        server.setHandleRequestBlock(handleRequest)
+        server.startListeningOnPort(0xB1e, withProtocolName: nil, andCompletionBlock: onCompletion)
+    }
+    
+    func pushToAll(data:NSData!){
+        server.pushToAll(data)
+    }
+}
