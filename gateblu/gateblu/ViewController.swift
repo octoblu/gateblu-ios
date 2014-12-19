46c0e56645ed42ae91689061bfe2c1e33904f9f0
Jade Meskill <jade.meskill@gmail.com>
2014-12-19 01:03:54 -0700

    add peripheral delegate, make discover actually work

--- gateblu/gateblu/ViewController.swift
+++ gateblu/gateblu/ViewController.swift
@@ -16,7 +16,7 @@ class NotificationScriptMessageHandler: NSObject, WKScriptMessageHandler {
   }
 }
 
-class ViewController: UIViewController, CBCentralManagerDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIWebViewDelegate {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIWebViewDelegate {
   
   var centralManager:CBCentralManager!
   var blueToothReady = false
@@ -67,6 +67,15 @@ class ViewController: UIViewController, CBCentralManagerDelegate, UICollectionVi
     return cell
   }
   
+  func derosenthal(uuid: String) -> String {
+    let regex = NSRegularExpression(pattern: "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", options: nil, error: nil)
+    var muuid = NSMutableString(string: uuid)
+    if countElements(uuid) <= 36 {
+      regex?.replaceMatchesInString(muuid, options: nil, range: NSMakeRange(0, countElements(uuid)), withTemplate: "$1-$2-$3-$4-$5")
+    }
+    return NSString(string: muuid).uppercaseString;
+  }
+  
   func startWebsocketServer() {
     let onCompletion = { (error: NSError!) -> Void in
       if ((error?) != nil) {
@@ -76,10 +85,8 @@ class ViewController: UIViewController, CBCentralManagerDelegate, UICollectionVi
     
     let handleRequest = { (data: NSData!) -> NSData! in
       let jsonResult = JSON(data: data)
-      let action = jsonResult["action"];
-      
-      println(action)
-      println(jsonResult)
+      println("Imma gonna \(jsonResult)")
+      let action = jsonResult["action"].stringValue
       
       switch action {
         case "startScanning":
@@ -95,25 +102,40 @@ class ViewController: UIViewController, CBCentralManagerDelegate, UICollectionVi
         
         case "stopScanning":
           self.stopDiscoveringDevices()
-          return data;
+          return data
         
         case "connect":
           self.connectToDevice(jsonResult["peripheralUuid"].stringValue)
-          return data;
+          return data
         
         case "discoverServices":
           var services = Array<String>()
           for uuid in jsonResult["uuids"].arrayValue {
-            services.append(uuid.stringValue)
+            services.append(self.derosenthal(uuid.stringValue))
           }
           self.discoverServices(jsonResult["peripheralUuid"].stringValue, services: services)
-          return data;
+          return data
         
         case "discoverCharacteristics":
-          self.discoverCharacteristics(jsonResult["peripheralUuid"].stringValue, serviceUuid: jsonResult["serviceUuid"].stringValue)
-          return data;
+          var characteristicUuids = Array<String>()
+          for uuid in jsonResult["characteristicUuids"].arrayValue {
+            characteristicUuids.append(self.derosenthal(uuid.stringValue))
+          }
+
+          self.discoverCharacteristics(jsonResult["peripheralUuid"].stringValue, serviceUuid: self.derosenthal(jsonResult["serviceUuid"].stringValue), characteristicUuids: characteristicUuids)
+          return data
+        
+        case "updateRssi":
+          self.updateRssi(jsonResult["peripheralUuid"].stringValue)
+          return data
+        
+        case "write":
+          let ddata = NSData(base64EncodedString: jsonResult["data"].stringValue, options: nil)!
+          self.write(jsonResult["peripheralUuid"].stringValue, serviceUuid: self.derosenthal(jsonResult["serviceUuid"].stringValue), characteristicUuid: self.derosenthal(jsonResult["characteristicUuid"].stringValue), data: ddata)
+          return data
         
         default:
+          println("I can't even \(action) with \(jsonResult)")
           return data
       }
     }
@@ -164,21 +186,15 @@ class ViewController: UIViewController, CBCentralManagerDelegate, UICollectionVi
   
   func discoverDevices(serviceUUIDs: Array<String>) {
     println("discovering devices")
-    let regex = NSRegularExpression(pattern: "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", options: nil, error: nil)
     var uuids = Array<CBUUID>()
     for uuid in serviceUUIDs {
-      var muuid = NSMutableString(string: uuid)
-      if countElements(uuid) <= 36 {
-        regex?.replaceMatchesInString(muuid, options: nil, range: NSMakeRange(0, countElements(uuid)), withTemplate: "$1-$2-$3-$4-$5")
-      }
-      uuids.append(CBUUID(string: muuid))
+      uuids.append(CBUUID(string: self.derosenthal(uuid)))
     }
     
     centralManager.scanForPeripheralsWithServices(uuids, options: nil)
   }
   
   func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
-    peripheral.discoverServices(nil)
     let data:JSON = [
       "type": "connect",
       "peripheralUuid": peripheral.identifier.UUIDString
@@ -198,32 +214,127 @@ class ViewController: UIViewController, CBCentralManagerDelegate, UICollectionVi
   
   func discoverServices(identifier: NSString, services: Array<String>) {
     let peripheral:CBPeripheral = self.foundPeripherals[identifier]!
+    var cbServices = Array<CBUUID>()
+    for service in services {
+      cbServices.append(CBUUID(string: service))
+    }
+    peripheral.discoverServices(cbServices);
+  }
+  
+  func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+    var services = Array<String>()
+    for service in peripheral.services {
+      let s = service as CBService
+      services.append(s.UUID.UUIDString)
+    }
     let data:JSON = [
       "type": "servicesDiscover",
-      "peripheralUuid": identifier,
+      "peripheralUuid": peripheral.identifier.UUIDString,
       "serviceUuids" : services
-    ]
-    self.server.pushToAll(data.rawData());
-  }
-
-  
-  func discoverCharacteristics(identifier: NSString, serviceUuid: NSString) {
-    let peripheral:CBPeripheral = self.foundPeripherals[identifier]!
-    let data:JSON = [
-      "type": "characteristicsDiscover",
-      "peripheralUuid": identifier,
-      "serviceUuid" : serviceUuid,
-      "characteristics": []
     ]
     println(data)
     self.server.pushToAll(data.rawData());
   }
   
+  func updateRssi(identifier: NSString) {
+    let peripheral:CBPeripheral = self.foundPeripherals[identifier]!
+    println("Reading RSSI")
+    peripheral.readRSSI()
+  }
+  
+  func peripheralDidUpdateRSSI(peripheral: CBPeripheral!, error: NSError!) {
+    let data:JSON = [
+      "type": "rssiUpdate",
+      "peripheralUuid": peripheral.identifier.UUIDString,
+      "rssi" : peripheral.RSSI
+    ]
+    println(data)
+    self.server.pushToAll(data.rawData());
+  }
+
+  
+  func discoverCharacteristics(identifier: NSString, serviceUuid: NSString, characteristicUuids: Array<String>) {
+    let peripheral:CBPeripheral = self.foundPeripherals[identifier]!
+    var foundService:CBService!
+    for service in peripheral.services {
+      let s = service as CBService
+      if s.UUID.UUIDString == serviceUuid {
+        foundService = s
+      }
+    }
+    
+    var cbUuids = Array<CBUUID>()
+    for uuid in characteristicUuids {
+      cbUuids.append(CBUUID(string: uuid))
+    }
+    peripheral.discoverCharacteristics(cbUuids, forService: foundService)
+  }
+  
+  func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
+  
+    var characteristics = Array<AnyObject>()
+    
+    for characteristic in service.characteristics {
+      let c = characteristic as CBCharacteristic
+      var properties = Array<String>()
+      
+      var descriptors = c.descriptors
+      if c.descriptors == nil {
+        descriptors = Array<CBDescriptor>()
+      }
+
+      for descriptor in descriptors {
+        let d = descriptor as CBDescriptor
+        properties.append(d.description)
+      }
+      
+      let ddata = [
+        "uuid":c.UUID.UUIDString,
+        "properties": properties
+      ]
+      characteristics.append(ddata);
+    }
+    
+    var data:JSON = [
+      "type": "characteristicsDiscover",
+      "peripheralUuid": peripheral.identifier.UUIDString,
+      "serviceUuid" : service.UUID.UUIDString,
+      "characteristics": characteristics
+    ]
+
+
+    println(data)
+    self.server.pushToAll(data.rawData());
+  }
+  
+  
+  func write(identifier: NSString, serviceUuid: NSString, characteristicUuid: NSString, data: NSData) {
+    let peripheral:CBPeripheral = self.foundPeripherals[identifier]!
+    
+    var foundService:CBService!
+    for service in peripheral.services {
+      let s = service as CBService
+      if s.UUID.UUIDString == serviceUuid {
+        foundService = s
+      }
+    }
+    
+    var foundCharacteristic:CBCharacteristic!
+    for characteristic in foundService.characteristics {
+      let c = characteristic as CBCharacteristic
+      if c.UUID.UUIDString == characteristicUuid {
+        foundCharacteristic = c
+      }
+    }
+    
+    peripheral.writeValue(data, forCharacteristic: foundCharacteristic, type: CBCharacteristicWriteType(rawValue: 1)!)
+  }
+  
   func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+    peripheral.delegate = self
     if peripheral.name != nil {
       let identifier = peripheral.identifier.UUIDString
       println("Discovered \(peripheral.name) \(identifier)")
-      peripheral.discoverServices(nil)
       self.foundPeripherals[identifier] = peripheral
 
       var services = peripheral.services
