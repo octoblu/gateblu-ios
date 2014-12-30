805dda4117a802b0fc40b05eb605d59624467d72
Aaron Herres and Peter DeMartini <sqrt+aaron+peter@octoblu.com>
2014-12-30 12:07:18 -0700

    Making progress

--- gateblu/gateblu/ViewController.swift
+++ gateblu/gateblu/ViewController.swift
@@ -16,6 +16,10 @@ class NotificationScriptMessageHandler: NSObject, WKScriptMessageHandler {
   }
 }
 
+let TOP_BAR_MARGIN : CGFloat = 60
+let CELL_PADDING : CGFloat = 5
+let CELL_HEIGHT : CGFloat = 160
+
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIWebViewDelegate {
   
   var deviceCollectionView : UICollectionView?
@@ -23,13 +27,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
   var cellSize : CGSize!
 
   override func viewDidLoad() {
-      super.viewDidLoad()
-      self.cellSize = CGSize(width: (self.view.bounds.width / 2) - 10, height: 120)
-      println("Starting Manager")
-      let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
-      deviceManager = appDelegate.deviceManager
-      deviceManager.start()
-      startDeviceCollectionView()
+    super.viewDidLoad()
+    NSLog("Starting Manager")
+    let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+    deviceManager = appDelegate.deviceManager
+    deviceManager.start()
+    startDeviceCollectionView()
   }
 
   override func didReceiveMemoryWarning() {
@@ -41,9 +44,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     let deviceWidth = self.view.bounds.width
     let deviceHeight = self.view.bounds.height
     
-    let frame = CGRect(x: 0, y: 60, width: deviceWidth, height: deviceHeight - 60)
+    let frame = CGRect(x: 0, y: TOP_BAR_MARGIN, width: deviceWidth, height: deviceHeight - TOP_BAR_MARGIN)
     let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-    layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    layout.sectionInset = UIEdgeInsets(top: CELL_PADDING, left: CELL_PADDING, bottom: CELL_PADDING, right: CELL_PADDING)
+    let cellWidth : CGFloat = (frame.width / 2) - (CELL_PADDING * 2)
+    self.cellSize = CGSize(width: cellWidth, height: CELL_HEIGHT)
     layout.itemSize = self.cellSize
     deviceCollectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
     deviceCollectionView!.delegate = self
@@ -60,70 +65,53 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
   
   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
     let cell = deviceCollectionView!.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
-    cell.backgroundColor = UIColor.grayColor()
-    cell.addSubview(createDeviceView(cell, indexPath: indexPath))
+    cell.backgroundColor = UIColor.lightGrayColor()
+    let deviceView = createDeviceView(cell, indexPath: indexPath)
+    let width = NSLayoutConstraint(item: deviceView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: cell, attribute: NSLayoutAttribute.Width, multiplier: 1, constant: 0)
+    let height = NSLayoutConstraint(item: deviceView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: cell, attribute: NSLayoutAttribute.Height, multiplier: 1, constant: 0)
+    
+    cell.addSubview(deviceView)
+    
+    cell.addConstraint(width)
+    cell.addConstraint(height)
+    
     return cell
   }
   
   func createDeviceView(cellView: UICollectionViewCell, indexPath: NSIndexPath) -> UIView {
-    let height = cellView.bounds.height
-    let width = cellView.bounds.width
-    let deviceView = UIView(frame: cellView.frame)
+    let height = cellSize.height
+    let width = cellSize.width
+    let deviceView = UIView()
+    
     let device: Device? = deviceManager.devices[indexPath.item]
     NSLog("Adding Device view \(device!.name)")
     if device == nil {
       return deviceView
     }
-    let deviceImage: UIImage! = getDeviceImage(device)
-//    deviceView.backgroundColor = UIColor(patternImage: deviceImage)
-    let deviceImageView: UIImageView = UIImageView(image: deviceImage)
+    let deviceImage: UIImage! = UIImage(named: device!.getImagePath())
+    let deviceImageView: UIImageView! = UIImageView(image: deviceImage)
+    deviceImageView.setTranslatesAutoresizingMaskIntoConstraints(false)
     deviceImageView.backgroundColor = UIColor.clearColor()
-    deviceView.addSubview(deviceImageView)
-    let labelHeight = (height / 4)
-    let deviceLabelFrame = CGRect(x: 0, y: height -  (labelHeight + 10) , width: width - 10, height: labelHeight)
-    let deviceLabel = UITextView(frame: deviceLabelFrame)
     
+    let centerx = NSLayoutConstraint(item: deviceImageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: deviceView, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)
+    let deviceTop = NSLayoutConstraint(item: deviceImageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: deviceView, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 10)
+    
+    deviceView.setTranslatesAutoresizingMaskIntoConstraints(false)
+    deviceView.addSubview(deviceImageView)
+    
+    deviceView.addConstraint(centerx)
+//    deviceView.addConstraint(deviceTop)
+    
+    let deviceLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.cellSize.width, height: 20))
     deviceLabel.text = device!.name
+    deviceLabel.textAlignment = NSTextAlignment.Center
     deviceLabel.textColor = UIColor.darkGrayColor()
-    deviceLabel.backgroundColor = UIColor.clearColor()
     
     deviceView.addSubview(deviceLabel)
-    
+  
+    let labelTop = NSLayoutConstraint(item: deviceLabel, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: deviceImageView, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 10)
+    deviceView.addConstraint(labelTop)
     return deviceView
-  }
-  
-  func imageResize (imageObj:UIImage, sizeChange:CGSize)-> UIImage{
-    
-    let hasAlpha = false
-    let scale: CGFloat = 0.0 // Automatically use scale factor of main screen
-    
-    UIGraphicsBeginImageContextWithOptions(sizeChange, !hasAlpha, scale)
-    imageObj.drawInRect(CGRect(origin: CGPointZero, size: sizeChange))
-    
-    let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
-    return scaledImage
-  }
-  
-  func getDeviceImage(device : Device!) -> UIImage {
-    var heightScale : CGFloat
-    var widthScale : CGFloat
-    
-    let file = device.getImagePath()
-    var iconImage : UIImage! = UIImage(named: file)
-
-    if iconImage.size.height > iconImage.size.width {
-      heightScale = 1
-      widthScale = iconImage.size.width / iconImage.size.height
-    } else {
-      widthScale = 1
-      heightScale = iconImage.size.height / iconImage.size.width
-    }
-    let widthSize = widthScale * self.cellSize.width
-    let heightSize = heightScale * self.cellSize.height
-    
-    NSLog("device dimensions: \(widthSize) x \(heightSize)")
-    NSLog("cell dimensions: \(self.cellSize.width) x \(self.cellSize.height)")
-    return imageResize(iconImage, sizeChange: CGSize(width: widthSize, height: heightSize))
   }
   
 }
